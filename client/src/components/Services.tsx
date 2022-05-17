8c07cf4bfd7b7230644e2bc8103591a884b65f02
Adilson Oliveira <adilsonreisoliveira@hotmail.com>
2022-05-17 01:07:02 -0300

    style: correct styked

--- client/src/components/Services.tsx
+++ client/src/components/Services.tsx
@@ -26,8 +26,8 @@ const ServiceCard = ({ color, title, icon, subtitle }: ServiceCardProps) => {
 
 export const Services: React.FC = () => {
   return (
-    <div className="flex flex-col md:flex-row w-full justify-center items-center gradient-bg-services">
-      <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+    <div className="flex flex-col md:flex-row w-full justify-center items-center gradient-bg-services px-20">
+      <div className="flex mf:flex-row flex-col items-center justify-between md:pl-20 py-12 px-4">
         <div className="flex-1 flex flex-col justify-start items-start">
           <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient">
             Services that we
@@ -35,26 +35,27 @@ export const Services: React.FC = () => {
             continue to improve
           </h1>
         </div>
-      </div>
-      <div className="flex-1 flex flex-col justify-start items-center">
-        <ServiceCard
-          color="bg-[#2952e3]"
-          title="Security Guaranteed"
-          icon={<BsShieldFillCheck fontSize={21} className="text-white" />}
-          subtitle="Security is guaranteed. We always maintain privacy and maintain the quality of our products."
-        />
-        <ServiceCard
-          color="bg-[#8945F8]"
-          title="Best exchange rates"
-          icon={<BiSearchAlt fontSize={21} className="text-white" />}
-          subtitle="Security is guaranteed. We always maintain privacy and maintain the quality of our products."
-        />
-        <ServiceCard
-          color="bg-[#f84550]"
-          title="Fastest transactions"
-          icon={<RiHeart2Fill fontSize={21} className="text-white" />}
-          subtitle="Security is guaranteed. We always maintain privacy and maintain the quality of our products."
-        />
+        <div className="flex-1 flex flex-col justify-start items-center">
+          <ServiceCard
+            color="bg-[#2952e3]"
+            title="Security Guaranteed"
+            icon={<BsShieldFillCheck fontSize={21} className="text-white" />}
+            subtitle="Security is guaranteed. We always maintain privacy and maintain the quality of our products."
+          />
+          <ServiceCard
+            color="bg-[#8945F8]"
+            title="Best exchange rates"
+            icon={<BiSearchAlt fontSize={21} className="text-white" />}
+            subtitle="Security is guaranteed. We always maintain privacy and maintain the quality of our products."
+          />
+          <ServiceCard
+            color="bg-[#f84550]"
+            title="Fastest transactions"
+            icon={<RiHeart2Fill fontSize={21} className="text-white" />}
+            subtitle="Security is guaranteed. We always maintain privacy and maintain the quality of our products."
+          />
+        </div>
+
       </div>
     </div>
   );
